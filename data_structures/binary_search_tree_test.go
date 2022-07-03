28c0638c127b4f56170ed4bba2d4d86345955be0
the-code-genin <adekunle3317@gmail.com>
2022-07-03 23:55:52 +0100

    Data deletion from the binary search tree works

--- data_structures/binary_search_tree_test.go
+++ data_structures/binary_search_tree_test.go
@@ -72,3 +72,52 @@ func TestBinarySearchTreeUpdating(t *testing.T) {
 		t.Errorf("expected %v, got %v", string(updateData), string(res))
 	}
 }
+
+func TestBinarySearchTreeDeleting(t *testing.T) {
+	cases := []struct {
+		key  string
+		data string
+	}{
+		{"hello", "world"},
+		{"foo", "bar"},
+		{"mohammed", "adekunle"},
+		{"lorem", "ipsum"},
+		{"code", "genin"},
+		{"genghis", "khan"},
+	}
+
+	// Initialize the tree
+	tree := NewBinarySearchTree()
+	for _, tc := range cases {
+		if err := tree.Insert([]byte(tc.key), []byte(tc.data)); err != nil {
+			t.Error(err)
+		}
+	}
+
+	// Delete from the tree
+	deleteKey := []byte("lorem")
+	err := tree.Delete(deleteKey)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Confirm the data update
+	_, err = tree.Get(deleteKey)
+	if err == nil {
+		t.Errorf("data deletion from the tree does not work as expected")
+	}
+
+	// Confirm the remaining key data matches while retrieving
+	for _, tc := range cases {
+		if tc.key == "lorem" { // Already deleted data
+			continue
+		}
+
+		res, err := tree.Get([]byte(tc.key))
+		if err != nil {
+			t.Error(err)
+		} else if string(res) != tc.data {
+			t.Errorf("expected %v, got %v", tc.data, string(res))
+		}
+	}
+}
