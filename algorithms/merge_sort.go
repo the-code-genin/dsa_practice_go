add6e389c9fc5147a80368b105689131fc811801
the-code-genin <adekunle3317@gmail.com>
2022-10-23 15:04:11 +0100

    Implemented merge sort algorithm

--- algorithms/merge_sort.go
+++ algorithms/merge_sort.go
@@ -1,9 +1,51 @@
 package algorithms
 
-type MergeSort struct {}
+import (
+	"math"
+)
 
-func (s MergeSort) SortInt(input []int) []int {
-	output := input
+type MergeSort struct{}
+
+func (s MergeSort) SortInt(partA, partB []int) []int {
+	output := make([]int, 0)
+
+	// Atomic sub array
+	if len(partA) == 0 && len(partB) == 0 {
+		return output
+	} else if len(partA) == 1 && len(partB) == 0 {
+		return partA
+	} else if len(partB) == 1 && len(partA) == 0 {
+		return partB
+	}
+
+	// Sort part a
+	midPointA := int(math.Ceil(float64(len(partA)) / 2))
+	sortedA := s.SortInt(partA[:midPointA], partA[midPointA:])
+
+	// Sort part b
+	midPointB := int(math.Ceil(float64(len(partB)) / 2))
+	sortedB := s.SortInt(partB[:midPointB], partB[midPointB:])
+
+	// Merge arrays
+	for i, j := 0, 0; i < len(sortedA) || j < len(sortedB); {
+		currA := math.MaxInt
+		if i < len(sortedA) {
+			currA = sortedA[i]
+		}
+
+		currB := math.MaxInt
+		if j < len(sortedB) {
+			currB = sortedB[j]
+		}
+
+		if currA <= currB {
+			output = append(output, currA)
+			i++
+		} else {
+			output = append(output, currB)
+			j++
+		}
+	}
 
 	return output
 }
--- algorithms/merge_sort_test.go
+++ algorithms/merge_sort_test.go
@@ -12,8 +12,9 @@ func TestMergeSort(t *testing.T) {
 		{[]int{9, 2, 5, 6, 1, 8}, []int{1, 2, 5, 6, 8, 9}},
 	}
 
+	empty := make([]int, 0)
 	for _, tc := range testCases {
-		res := MergeSort{}.SortInt(tc.in)
+		res := MergeSort{}.SortInt(tc.in, empty)
 		if len(res) != len(tc.out) {
 			t.Errorf("%v was expected to match %v", res, tc.out)
 		}
